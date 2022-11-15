17aeb845987aba2a1618223ab50ada93de8b39ec
Nitesh Shah <Fineoutput2021>
2022-11-15 19:07:42 +0530

    booking details updates

--- application/views/frontend/index.php
+++ application/views/frontend/index.php
@@ -1057,26 +1057,26 @@
 
 
 	<script>
-		var galleryTop = new Swiper('.gallery-top', {
-			spaceBetween: 0,
-			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev',
-			},
-			loop: true,
-			loopedSlides: 4
-		});
-		var galleryThumbs = new Swiper('.gallery-thumbs', {
-			spaceBetween: 10,
-			centeredSlides: true,
-			slidesPerView: 'auto',
-			touchRatio: 0.2,
-			slideToClickedSlide: true,
-			loop: true,
-			loopedSlides: 4
-		});
-		galleryTop.controller.control = galleryThumbs;
-		galleryThumbs.controller.control = galleryTop;
+		// var galleryTop = new Swiper('.gallery-top', {
+		// 	spaceBetween: 0,
+		// 	navigation: {
+		// 		nextEl: '.swiper-button-next',
+		// 		prevEl: '.swiper-button-prev',
+		// 	},
+		// 	loop: true,
+		// 	loopedSlides: 4
+		// });
+		// var galleryThumbs = new Swiper('.gallery-thumbs', {
+		// 	spaceBetween: 10,
+		// 	centeredSlides: true,
+		// 	slidesPerView: 'auto',
+		// 	touchRatio: 0.2,
+		// 	slideToClickedSlide: true,
+		// 	loop: true,
+		// 	loopedSlides: 4
+		// });
+		// galleryTop.controller.control = galleryThumbs;
+		// galleryThumbs.controller.control = galleryTop;
 		//---------- Counter code start ---------------------------
 		$.fn.jQuerySimpleCounter = function(options) {
 			var settings = $.extend({
