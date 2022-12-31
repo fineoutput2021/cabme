e5791ddfec920f0759b7566fc197527c490054d2
Nitesh Shah <Fineoutput2021>
2022-12-31 18:28:19 +0530

    my booking updates

--- application/controllers/Apicontroller/Homecontroller.php
+++ application/controllers/Apicontroller/Homecontroller.php
@@ -68,4 +68,42 @@ class Homecontroller extends CI_Controller
         );
         echo json_encode($res);
     }
+    public function my_booking($type)
+    {
+        $header = $this->input->request_headers();
+        $auth = $header['Authorization'];
+        $user_data = $this->db->get_where('tbl_users', array('is_active' => 1, 'auth' => $auth))->result();
+        if (!empty($user_data)) {
+            $booking_data = $this->db->get_where('tbl_booking', array('order_status' => $type, 'user_id' => $user_data[0]->id))->result();
+            $data = [];
+            foreach ($booking_data as $booking) {
+                if ($booking->booking_type == 1) {
+                    $booking_type = 'Self Drive';
+                } else if ($booking->booking_type == 2) {
+                    $booking_type = 'Intercity';
+                } else {
+                    $booking_type = 'Out Station';
+                }
+                $newdate = new DateTime($booking->date);
+                $date = $newdate->format('d/m/Y');   #d-m-Y  // March 10, 2001, 5:16 pm
+                $data[] = array(
+                    'id' => $booking->id,
+                    'booking_type' => $booking_type,
+                    'date' => $date,
+                );
+            }
+            $res = array(
+                'message' => "success",
+                'status' => 200,
+                'data' => $data
+            );
+            echo json_encode($res);
+        } else {
+            $res = array(
+                'message' => 'Permission Denied!',
+                'status' => 201
+            );
+            echo json_encode($res);
+        }
+    }
 }
