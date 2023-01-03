4a78022660e16aa86f422a3269cf15284c0ab758
Nitesh Shah <Fineoutput2021>
2023-01-03 21:35:07 +0530

    error fix3

--- application/controllers/Apicontroller/Apicontroller.php
+++ application/controllers/Apicontroller/Apicontroller.php
@@ -557,7 +557,7 @@ class Apicontroller extends CI_Controller
                 'transmission' => $transmission,
                 'seating' => $seating,
                 'extra_kilo' => $car[0]->extra_kilo,
-                'kilometer' => $$booking_data[0]->kilometer,
+                'kilometer' => $booking_data[0]->kilometer,
                 'total_amount' => $booking_data[0]->total,
                 'final_amount' => $booking_data[0]->final_amount,
                 'rsda' => $booking_data[0]->rsda,
